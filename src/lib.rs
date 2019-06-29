17f9d8483737b0e9d5435b7dce73330a743a2f6c
Matthew Plant <maplant95@gmail.com>
2019-06-29 14:54:05 -0700

    Add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+// Copyright 2019 The Aljabar Developers. For a full listing of authors,
+// refer to the Cargo.toml file at the top-level directory of this distribution.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 //! The super generic super experimental linear algebra library.
 //!
 //! This library serves the dual purpose of being an experimental API for
