fccc9dc5599789a361762ef5f8a2b30616aca75f
Drake Tetreault <ekardnt@ekardnt.com>
2019-08-07 19:17:47 -0700

    Implement reflect. (#6)
    * Implement reflect.
    
    * Simplify reflection calculation.

--- src/lib.rs
+++ src/lib.rs
@@ -1355,6 +1355,15 @@ where
     fn magnitude2(self) -> Self::Scalar {
         self.clone().dot(self)
     }
+
+    /// Returns the [reflection](https://en.wikipedia.org/wiki/Reflection_(mathematics))
+    /// of the current vector with respect to the given surface normal. The surface normal
+    /// must be of length 1 for the return value to be correct. The current vector is
+    /// interpreted as pointing toward the surface, and does not need to be normalized.
+    fn reflect(self, surface_normal: Self) -> Self {
+        let a = surface_normal.clone() * self.clone().dot(surface_normal);
+        self - (a.clone() + a)
+    }
 }
 
 /// Defines an InnerSpace where the Scalar is a real number. Automatically
@@ -2921,6 +2930,20 @@ mod tests {
         assert_eq!(2.0, v.y());
         assert_eq!(3.0, v.z());
         assert_eq!(4.0, v.w());
+    }
+
+    fn test_reflect() {
+        // Incident straight on to the surface.
+        let v = vec2(1, 0);
+        let n = vec2(-1, 0);
+        let r = v.reflect(n);
+        assert_eq!(r, vec2(-1, 0));
+
+        // Incident at 45 degree angle to the surface.
+        let v = vec2(1, 1);
+        let n = vec2(-1, 0);
+        let r = v.reflect(n);
+        assert_eq!(r, vec2(-1, 1));
     }
 
     #[test]
