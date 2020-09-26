b6a76e29435245c00a0d430e5ac9644105f7289e
Øystein Hovind <oystein.hovind@folq.no>
2020-09-26 18:12:08 +0200

    Add acos and asin

--- src/lib.rs
+++ src/lib.rs
@@ -344,6 +344,12 @@ where
     /// Returns the tangent of the angle.
     fn tan(self) -> Self;
 
+    /// Returns the arcsine of the angle.
+    fn asin(self) -> Self;
+
+    /// Returns the arccos of the angle.
+    fn acos(self) -> Self;
+
     /// Returns the four quadrant arctangent of `self` and `x` in radians.
     fn atan2(self, x: Self) -> Self;
 
@@ -374,6 +380,14 @@ impl Real for f32 {
 
     fn cos(self) -> Self {
         self.cos()
+    }
+
+    fn asin(self) -> Self {
+        self.asin()
+    }
+
+    fn acos(self) -> Self {
+        self.acos()
     }
 
     fn tan(self) -> Self {
@@ -412,6 +426,14 @@ impl Real for f64 {
 
     fn cos(self) -> Self {
         self.cos()
+    }
+
+    fn asin(self) -> Self {
+        self.asin()
+    }
+
+    fn acos(self) -> Self {
+        self.acos()
     }
 
     fn tan(self) -> Self {
